110eb6161adff297bbeaf969b7d52fe432cff963
Swapnil Bhosale <bhosaleswapnil@gmail.com>
2024-01-13 17:54:56 -0800

    First

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the AWS HomePage!")
+    fmt.Fprintf(w, "Welcome to the AWS HomePage")
     fmt.Println("Endpoint Hit: homePage")
 }
 
